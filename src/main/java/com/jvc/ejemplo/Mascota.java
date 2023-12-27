14dbe73a8f448eb159b380ed237bd5703ada14b1
Jovace8 <josue.vargas0912@gmail.com>
2023-12-27 15:33:34 -0600

    clase mascota añadida

---
+++ src/main/java/com/jvc/ejemplo/Mascota.java
@@ -0,0 +1,51 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.jvc.ejemplo;
+
+/**
+ *
+ * @author josue
+ */
+public class Mascota {
+    
+    private int id ;
+    private String nombre;
+    private String raza;
+
+    public Mascota() {
+    }
+
+    public Mascota(int id, String nombre, String raza) {
+        this.id = id;
+        this.nombre = nombre;
+        this.raza = raza;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getRaza() {
+        return raza;
+    }
+
+    public void setRaza(String raza) {
+        this.raza = raza;
+    }
+    
+    
+}
